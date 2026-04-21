1fb6d573ac1311ec1f48f1240ce91a04e6d46ee5
Arthur Phillip Silva <artphil.dev@gmail.com>
2026-04-21 12:40:08 -0300

    fix/ inconsistents to build

--- src/utils/search.ts
+++ src/utils/search.ts
@@ -63,21 +63,21 @@ export default class SearchIndex {
 
     const matrix = Array.from({ length: rows }, () => new Array(cols).fill(0))
 
-    for (let i = 0; i < rows; i++) matrix[i][0] = i
-    for (let j = 0; j < cols; j++) matrix[0][j] = j
+    for (let i = 0; i < rows; i++) matrix[i]![0] = i
+    for (let j = 0; j < cols; j++) matrix[0]![j] = j
 
     for (let i = 1; i < rows; i++) {
       for (let j = 1; j < cols; j++) {
         const cost = a[i - 1] === b[j - 1] ? 0 : 1
 
-        matrix[i][j] = Math.min(
-          matrix[i - 1][j] + 1,
-          matrix[i][j - 1] + 1,
-          matrix[i - 1][j - 1] + cost,
+        matrix[i]![j] = Math.min(
+          matrix[i - 1]![j] + 1,
+          matrix[i]![j - 1] + 1,
+          matrix[i - 1]![j - 1] + cost,
         )
       }
     }
 
-    return matrix[rows - 1][cols - 1]
+    return matrix[rows - 1]![cols - 1]
   }
 }
