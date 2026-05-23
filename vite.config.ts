d54be9d0213211bd44631c348d35807e80f9d9e0
Arthur Phillip Silva <artphil.dev@gmail.com>
2026-05-23 00:38:00 -0300

    chore/clean

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
 
   return {
-    base: process.env.VITE_BASE_URL || env.VITE_BASE_URL || '/',
-    plugins: [vue(), vueJsx(), vueDevTools()],
+    base: env.VITE_BASE_URL || '/',
+    plugins: [vue()],
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url)),
