28e5627e28384780b3b59edbed277423b2af4131
Arthur Phillip Silva <artphil.dev@gmail.com>
2026-06-14 08:36:18 -0300

    feat/tags acessible

--- src/components/FeatureTag.vue
+++ src/components/FeatureTag.vue
@@ -1,5 +1,15 @@
+<template>
+  <span :class="['tag', isCorrect ? 'tag--correct' : 'tag--incorrect']">
+    <CheckIcon v-if="isCorrect" :aria-label="t('correct')" />
+    <CloseIcon v-else :aria-label="t('incorrect')" />
+    {{ t(text) }}
+  </span>
+</template>
+
 <script setup lang="ts">
+import CloseIcon from '@/assets/icons/CloseIcon.vue';
 import { t } from '../utils/translate';
+import CheckIcon from '@/assets/icons/CheckIcon.vue';
 defineProps({
   text: {
     type: String,
@@ -12,28 +22,31 @@ defineProps({
 })
 </script>
 
-<template>
-  <span :class="['tag', isCorrect ? 'tag--correct' : 'tag--incorrect']">
-    {{ t(text) }}
-  </span>
-</template>
-
-
 <style scoped>
 .tag {
-  display: inline-block;
+  --tag-color: var(--color-heading);
+
+  display: flex;
+  align-items: center;
+  gap: 4px;
   padding: 0.25rem 0.75rem;
   border-radius: var(--border-radius);
   font-size: 0.875rem;
-  font-weight: 500;
-  color: var(--color-heading);
+  font-weight: 700;
+  border: 2px solid var(--tag-color);
+  background-color: var(--color-background-cards);
+  color: var(--tag-color);
+}
+
+.tag svg {
+  fill: var(--tag-color);
 }
 
 .tag--correct {
-  background-color: var(--color-correct);
+  --tag-color: var(--color-correct);
 }
 
 .tag--incorrect {
-  background-color: var(--color-wrong);
+  --tag-color: var(--color-wrong);
 }
 </style>
